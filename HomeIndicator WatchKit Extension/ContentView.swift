d0bdd15b85cfee80bac44dd7747395ebbdef018d
Shuhei Shitamori <shitamori1272@gmail.com>
2020-08-09 16:39:12 +0900

    first viewを作成

--- HomeIndicator WatchKit Extension/ContentView.swift
+++ HomeIndicator WatchKit Extension/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let locationManager = CLLocationManager()
+    
+    @State var locationText = "自宅"
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("登録された場所")
+            Divider()
+            Text(locationText)
+            Divider()
+            HStack {
+                Button(action: {
+                    self.locationManager.requestWhenInUseAuthorization()
+                    self.locationText = String(describing: self.locationManager.location)
+                }) {
+                    Text("登録")
+                    Button(action: {
+                        print("test")
+                    }) {
+                        Image("icn_plus")
+                            .resizable()
+                    }
+                }
+            }
+        }
     }
 }
 
