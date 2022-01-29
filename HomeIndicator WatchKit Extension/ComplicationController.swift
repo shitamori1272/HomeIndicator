3671d98efddc34073f5171c87e280e50fb96ce9e
Shuhei Shitamori <shitamori1272@gmail.com>
2022-01-29 23:28:44 +0900

    不要なメソッドを削除

--- HomeIndicator WatchKit Extension/ComplicationController.swift
+++ HomeIndicator WatchKit Extension/ComplicationController.swift
@@ -14,18 +14,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     @ObservedObject var viewModel = ContentViewModel()
     
-    func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
-    }
-    
-    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
         handler(.showOnLockScreen)
     }
@@ -40,16 +28,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         } else {
             handler(nil)
         }
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries prior to the given date
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
     }
     
     // MARK: - Placeholder Templates
