7452e75b00d4cfbc6853630a5f4e83dd5c6a8d6c
shitamori1272 <shitamori1272@gmail.com>
2026-03-01 21:01:12 +0900

    Chagne to Folder structure

---
+++ HomeIndicatorTests/HomeIndicatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeIndicatorTests.swift
+//  HomeIndicatorTests
+//
+//  Created by Shuhei Shitamori on 2026/03/01.
+//  Copyright © 2026 Shitamori. All rights reserved.
+//
+
+import Testing
+
+struct HomeIndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
